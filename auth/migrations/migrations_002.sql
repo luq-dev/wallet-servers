c57ba1b3e4fd0d0b0fa1d9b46d71c4a08a9c833a
luq-dev <luqtilly@gmail>
2026-06-01 18:18:40 +0300

    had to move migrations out of the code directories

--- auth/migrations/migrations_002.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS account_types (
-    id SERIAL PRIMARY KEY,
-    type_name VARCHAR(50) NOT NULL,
-    currency VARCHAR(3)
-);
-
-ALTER TABLE accounts ADD COLUMN IF NOT EXISTS account_type INTEGER REFERENCES account_types(id) ON DELETE RESTRICT; 
