6c000bab4d9d1d29f0f3638c47f7762368151adc
calabash <calabashadventures@gmail.com>
2026-09-08 07:50:30 +0600

    refreshed the transaction service. I need to be more organized with these commits :-|

--- transaction/go.mod
+++ transaction/go.mod
@@ -2,14 +2,16 @@ module transaction
 
 go 1.26.4
 
-require user v0.0.0
+require (
+	storage v0.0.0
+	user v0.0.0
+)
 
-require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
 
 replace user => ../user
-
-replace message => ../message
-
-replace finance => ../finance
 
 replace storage => ../storage
--- transaction/services/message/iso8583.go
+++ transaction/services/message/iso8583.go
@@ -6,41 +6,56 @@ import (
 )
 
 type ISO8583 struct {
-	MTI string
-	Bitmap string
-	DE map[int]any
+	MTI    []byte
+	Bitmap int64
+	DE     map[int][]byte
 }
 
-func (msg *ISO8583) SetMTI(mti string) {
-	msg.MTI = mti
-}
+func (msg *ISO8583) Encode() []byte {
 
-func (msg *ISO8583) SetDE(n int, value any){
-	msg.DE[n] = value
-}
-
-func (msg *ISO8583) Build() string {
-	
 	var bitmap int64
-	var DEs []any
+	var DEs [][]byte
 	var msgString string
+
+	var buff []byte
 
 	for k, _ := range msg.DE {
 		bitmap += int64(math.Pow(2, float64(64-k)))
 	}
 
-	for i := 1; i<=64; i++ {
+	for i := 1; i <= 64; i++ {
 		if _, exists := msg.DE[i]; exists {
 			DEs = append(DEs, msg.DE[i])
 		}
 	}
 
-	msgString = fmt.Sprintf("%s\n", msg.MTI)
+	buff = append(buff, msg.MTI...)
+	buff = append(buff)
+
+	msgString = fmt.Sprintf("%d\n", int(msg.MTI))
 	msgString = fmt.Sprintf("%s%x\n", msgString, bitmap)
 
-	for n := range DEs{
-		msgString = fmt.Sprintf("%s%s\n", msgString,DEs[n])
+	for n := range DEs {
+		msgString = fmt.Sprintf("%s%s\n", msgString, DEs[n])
 	}
 
-	return msgString
+	return []byte(msgString)
+}
+
+func NewMessage() *ISO8583 {
+	return &ISO8583{}
+}
+
+func (msg *ISO8583) SetMTI(mti []byte) error {
+
+	if len(mti) != 4 {
+		return fmt.Errorf("Invalid MTI length")
+	}
+
+	msg.MTI = mti
+	return nil
+}
+
+func (msg *ISO8583) SetDE(n int, value []byte) {
+	msg.DE[n] = value
 }
